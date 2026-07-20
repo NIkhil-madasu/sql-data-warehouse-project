5fadad96d7f0ef57a3a6bbb8821b47f69064b4cd
NIkhil-madasu <madasunikhil2000@gmail.com>
2026-07-20 17:18:47 +0530

    Update SQL script to find customer ages and order dates

--- scripts/daterange_exploration.sql
+++ scripts/daterange_exploration.sql
@@ -5,11 +5,18 @@
 --SYNTAX= MAX/MIN[DATE DIM]
 --EXAMPLE: MAX/MIN [ORDER_DATE],MAX/MIN[BIRTH_DATE] ,MAX/MIN[CRATE_DATE]
 
--------------FIND THE YOUNGEST AND OLDEST EMPLOYEES AND FIND THEIR AGES-----------
-
+-------------FIND THE YOUNGEST AND OLDEST CUSTOMESR AND FIND THEIR AGES-------------------
 SELECT
-MIN(BirthDate) AS OLDEST_EMPLOYEE,
-DATEDIFF(YEAR,MIN(BirthDate),GETDATE()) AS  OLDEST_AGE ,
-MAX(BirthDate) AS YOUNGEST_EMPLOYEE,
-DATEDIFF(YEAR,MAX(BirthDate),GETDATE()) AS  YOUNGEST_AGE 
-FROM Sales.Employees
+Min(Birthdate) as old_customer_bdate,
+datediff(year,Min(Birthdate),GETDATE())as  old_customer_age,
+Max(Birthdate) as young_customer_bdate,
+datediff(year,max(Birthdate),GETDATE())as  young_customer_age
+FROM gold.dim_Customers
+
+
+--FIND THE DATE OF FIRST AND LAST ORDER  AND ALSO FIND HOW MANY YEARS OF SALES ARE AVAILABLE--
+SELECT
+MIN(Order_Date) AS First_order_date,
+MAX(Order_Date) AS Last_order_date,
+DATEDIFF(year,MIN(Order_Date),MAX(Order_Date))as Order_range_Year
+FROM gold.fact_sales

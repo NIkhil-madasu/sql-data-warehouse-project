269c4b2a6df6e2278c4d56d0dc226d0bf22c9d19
NIkhil-madasu <madasunikhil2000@gmail.com>
2026-07-20 17:20:10 +0530

    Refactor SQL queries for business metrics reporting
    Updated SQL queries to retrieve various metrics from the gold schema, including total sales, items sold, orders, products, and customers.

--- scripts/measure_exploration.sql
+++ scripts/measure_exploration.sql
@@ -5,13 +5,38 @@
 --SYNTAX = AGG[MEASURE]--
 --EXAMPLE: SUM(SALES),AVG(PRICE),SUM(QUANTITY),COUNT(SALES)---
 
---FIND TOTAL SALES
-SELECT SUM(Sales) as Total_sales FROM Sales.Orders
+-------FIND THE TOTAL SALES-------
+select sum(sales_amount)as total_sales from gold.fact_sales
 
---FIND HOW MANY ITEMS SOLD
-SELECT SUM(Quantity) AS  TOTAL_QUANTITY FROM Sales.Orders
+-------FIND HOW MANY ITEMS ARE SOLD------
+select sum(Quantity)as total_items from gold.fact_sales
 
---FIND TOTAL NO. OF ORDERS WITHOUT NULLS
-SELECT COUNT(ORDERID) AS TOTAL_ORDERS FROM Sales.Orders WHERE OrderID IS NOT NULL
--------WE CAN ALSO USE DISNTINCT-----------
-SELECT COUNT(DISTINCT ORDERID) AS TOTAL_ORDERS FROM Sales.Orders
+--------FIND AVG SELLING PRICE-----------
+select avg(price) as avg_pice from gold.fact_sales
+
+--------FIND THE TOTAL NUMBER OF ORDERS----------
+select count(distinct Order_Number) as total_orders from gold.fact_sales
+
+--------FIND THE TOTAL NUMBER OF PRODUCTS----------
+select count(distinct product_key) as total_products from gold.dim_Products
+
+--------FIND THE TOTAL NUMBER OF CUSTOMERS----------
+select count(distinct Customer_Key) as total_customers from gold.dim_Customers
+
+--------FIND THE TOTAL NUMBER OF CUSTOMERS THAT PLACED ORDER----------
+select count(distinct Customer_Key) as total_customers from gold.fact_sales
+
+--------GENERATE A REPORT TO SHO ALL KEY MATRICS OF OUR BUSSINESS------------
+select 'total_sales' as Measure_name ,sum(sales_amount)as Measure_value from gold.fact_sales
+union all
+select'total_items' as Measure_name, sum(Quantity)as Measure_value from gold.fact_sales
+union all
+select 'avg_pice',avg(price)  from gold.fact_sales
+union all
+select'total_orders', count(distinct Order_Number)  from gold.fact_sales
+union all
+select 'total_products',count(distinct product_key)  from gold.dim_Products
+union all
+select'total_customers', count(distinct Customer_Key)  from gold.dim_Customers
+union all
+select'total_customers', count(distinct Customer_Key)  from gold.fact_sales
